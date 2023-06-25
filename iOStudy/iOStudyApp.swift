56b1ee5e8fb873ce164e19ec49b90792553cedb7
qnquangg <quang.ng193@gmail.com>
2023-06-25 16:00:35 +0700

    Initial Commit

---
+++ iOStudy/iOStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOStudyApp.swift
+//  iOStudy
+//
+//  Created by Quang Nguyen on 06/25/23.
+//
+
+import SwiftUI
+
+@main
+struct iOStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
